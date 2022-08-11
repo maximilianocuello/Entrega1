538ae867f21e308b415797fe84b3bf439188640e
maximilianocuello <110405289+MaxiCuello07@users.noreply.github.com>
2022-08-11 09:01:29 -0300

    punto3 igualdad de contraseñas

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,19 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const submit = document.getElementById('regBtn');
+let password1 = document.getElementById('password1');
+let password2 = document.getElementById('password2');
+
+
+submit.addEventListener("click", function(e){
+    let password1 = document.getElementById('password1');
+    let password2 = document.getElementById('password2');
+    if(password1.value === password2.value){
+        showAlertSuccess()
+    }
+    else{
+        showAlertError()
+    }
+})
